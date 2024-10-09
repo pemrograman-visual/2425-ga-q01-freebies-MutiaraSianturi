a72a218529029df3c22ac4548d01a0a8ebde9464
MutiaraSianturi <mutiarasianturi006@gmail.com>
2024-10-09 16:06:49 +0700

    Pair 7

--- X01.java
+++ X01.java
@@ -1,2 +1,39 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24003 - Michael Nasution
+// 12S24045 - Mutiara Sianturi
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double termurah, price, total;
+
+        total = 0;
+        termurah = 999;
+        price = Double.parseDouble(input.nextLine());
+        while (price != 0) {
+            if (price == 0) {
+            } else {
+                if (price < termurah) {
+                    termurah = price;
+                } else {
+                    price = price;
+                }
+                total = total + price;
+            }
+            price = Double.parseDouble(input.nextLine());
+        }
+        if (total >= 100) {
+            total = total - termurah;
+        } else {
+            total = total;
+        }
+        System.out.println(toFixed(total,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
